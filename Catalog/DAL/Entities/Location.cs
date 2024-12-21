09ea7f637af0723aa138dff777098c5122b87b3e
LafRina <lafenkoirina5@gmail.com>
2024-12-21 03:06:08 +0200

    Added Entities to DAL

---
+++ Catalog/DAL/Entities/Location.cs
@@ -0,0 +1,8 @@
+namespace Catalog.DAL.Entities {
+    public class Location {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
---
+++ Catalog/DAL/Entities/Weather.cs
@@ -0,0 +1,10 @@
+namespace Catalog.DAL.Entities {
+    public class Weather {
+        public int Id { get; set; }
+        public float Temperature { get; set; }
+        public int Humidity { get; set; }
+        public int Pressure { get; set; }
+        public int LocationId { get; set; }
+        public int UserId { get; set; }
+    }
+}
