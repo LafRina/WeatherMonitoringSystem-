06d9ef53c59f2b3949d989f478d960945e74b783
LafRina <lafenkoirina5@gmail.com>
2024-12-30 01:15:11 +0200

    added new method

--- Catalog/DAL/Repositories/Impl/WeatherRepository.cs
+++ Catalog/DAL/Repositories/Impl/WeatherRepository.cs
@@ -8,5 +8,13 @@ namespace DAL.Repositories.Impl
     public class WeatherRepository : Repository<Weather>, IWeatherRepository
     {
         public WeatherRepository(DbContext context) : base(context) { }
+        
+        public async Task<Weather> GetWeatherByLocationAsync(int locationId)
+        {
+            return await _context.Set<Weather>() 
+                .Where(w => w.LocationId == locationId)
+                .OrderByDescending(w => w.Id) // Отримати останнє оновлення погоди
+                .FirstOrDefaultAsync();
+        }
     }
 }
