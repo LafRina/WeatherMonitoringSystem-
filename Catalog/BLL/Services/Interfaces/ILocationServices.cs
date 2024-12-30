026e8af95284e88e8a7542397bdaaeb266433358
LafRina <lafenkoirina5@gmail.com>
2024-12-30 03:08:28 +0200

    added interfaces and impl to services

---
+++ Catalog/BLL/Services/Interfaces/ILocationServices.cs
@@ -0,0 +1,11 @@
+
+using Catalog.BLL.DTO;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BLL.Services
+{
+    public interface ILocationServices : IServicess
+    {
+       
+    }
+}
